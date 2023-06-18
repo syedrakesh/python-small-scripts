a80eb38f0f768529d072ba2d1b718c21ca627243
syedrakesh <syedrakesh.me@gmail.com>
2023-06-18 12:01:33 +0600

    Work Order User Update Query Maker

---
+++ wo_user_update_query_maker.py
@@ -0,0 +1,32 @@
+import sys
+
+wo_responsible_user = 317    # Update number here only
+
+# Open a file for writing
+with open('wo_user_update_query_output.txt', 'w') as f:
+    # Redirect the standard output to the file
+    sys.stdout = f
+
+    # Print some output
+    print("UPDATE t_workorders dtw\nSET dtw.wo_responsible_user = ", wo_responsible_user, "\nWHERE")
+    # Open the file for reading
+    with open("wo_user_update_query_input.txt", "r") as file:
+        # Read all lines of the file into a list
+        lines = file.readlines()
+
+        # Loop through each line in the file
+        for line in lines:
+
+            # If this is not the last line, just print the line
+            if line != lines[-1]:
+                line = line.strip()
+                query = "(dtw.dam_wo_id = '{}') OR".format(line)
+                print(query)
+
+            # If this is the last line, print without OR
+            else:
+                print("(dtw.dam_wo_id = '{}');".format(line))
+
+    # Restore the standard output
+    sys.stdout = sys.__stdout__
+print("Query Successfully Generated In wo_user_update_query_output.txt")
